8f707c9c97fd74e344477449fa1e71830b41c282
team-ITUS <itusgithub@gmail.com>
2026-04-21 18:00:42 +0530

    razor pages commit

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]    // Data Annotation--  it is used to specifically define a primary key if the name is other that Id.
+        public int Id { get; set; }   // If name is Id, then it is automatically considered as primary key, but we can also explicitly specify it using [Key] attribute.
+        [Required]   // Data Annotation-- it is used  to specify that the property is required and cannot be null or empty.
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [Range(1, 100, ErrorMessage = "Display Order must be Between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
